388466119e9a3926d821bda94a138f646609a41b
WagnoLeaoSergio <wagnoleao@gmail.com>
2021-10-30 20:27:59 -0300

    Execution of News_extractor on main function, command parser commented

--- hacker_news_email_sender/__main__.py
+++ hacker_news_email_sender/__main__.py
@@ -1,6 +1,5 @@
 import argparse  # pragma: no cover
-
-from . import BaseClass, base_function  # pragma: no cover
+from . import newsExctractor
 
 
 def main() -> None:  # pragma: no cover
@@ -19,42 +18,46 @@ def main() -> None:  # pragma: no cover
         * List all available tasks
         * Run an application (Flask, FastAPI, Django, etc.)
     """
-    parser = argparse.ArgumentParser(
-        description="hacker_news_email_sender.",
-        epilog="Enjoy the hacker_news_email_sender functionality!",
-    )
+    # parser = argparse.ArgumentParser(
+        # description="hacker_news_email_sender.",
+        # epilog="Enjoy the hacker_news_email_sender functionality!",
+    # )
     # This is required positional argument
-    parser.add_argument(
-        "name",
-        type=str,
-        help="The username",
-        default="WagnoLeaoSergio",
-    )
+    # parser.add_argument(
+        # "name",
+        # type=str,
+        # help="The username",
+        # default="WagnoLeaoSergio",
+    # )
     # This is optional named argument
-    parser.add_argument(
-        "-m",
-        "--message",
-        type=str,
-        help="The Message",
-        default="Hello",
-        required=False,
-    )
-    parser.add_argument(
-        "-v",
-        "--verbose",
-        action="store_true",
-        help="Optionally adds verbosity",
-    )
-    args = parser.parse_args()
-    print(f"{args.message} {args.name}!")
-    if args.verbose:
-        print("Verbose mode is on.")
+    # parser.add_argument(
+        # "-m",
+        # "--message",
+        # type=str,
+        # help="The Message",
+        # default="Hello",
+        # required=False,
+    # )
+    # parser.add_argument(
+        # "-v",
+        # "--verbose",
+        # action="store_true",
+        # help="Optionally adds verbosity",
+    # )
+    # args = parser.parse_args()
+    # print(f"{args.message} {args.name}!")
+    # if args.verbose:
+        # print("Verbose mode is on.")
 
-    print("Executing main function")
-    base = BaseClass()
-    print(base.base_method())
-    print(base_function())
-    print("End of main function")
+    # print("Executing main function")
+    # base = BaseClass()
+    # print(base.base_method())
+    # print(base_function())
+    # print("End of main function")
+
+    extractor = newsExctractor.News_extractor()
+    extractor.extract_news()
+    extractor.send_email()
 
 
 if __name__ == "__main__":  # pragma: no cover
