655aeca82a5c5ea7a2c09436f348bce63097b55c
WagnoLeaoSergio <wagnoleao@gmail.com>
2021-10-31 23:20:54 -0300

    Parser added.
    - New required arguments: sender and receiver

--- hacker_news_email_sender/__main__.py
+++ hacker_news_email_sender/__main__.py
@@ -1,3 +1,4 @@
+import argparse
 from . import newsExctractor
 
 
@@ -6,21 +7,29 @@ def main() -> None:  # pragma: no cover
     The main function executes on commands:
     `python -m hacker_news_email_sender` and `$ hacker_news_email_sender `.
 
-    This is your program's entry point.
-
-    You can change this function to do whatever you want.
-    Examples:
-        * Run a test suite
-        * Run a server
-        * Do some other stuff
-        * Run a command line application (Click, Typer, ArgParse)
-        * List all available tasks
-        * Run an application (Flask, FastAPI, Django, etc.)
+    The program's entry point.
     """
 
-    extractor = newsExctractor.News_extractor()
-    extractor.extract_news()
-    extractor.send_email()
+    parser = argparse.ArgumentParser(
+        description='Extract and send the Hacker News to an email.'
+    )
+    parser.add_argument(
+        "sender",
+        type=str,
+        help='The email address used to send the email'
+    )
+
+    parser.add_argument(
+        "receiver",
+        type=str,
+        help='The email address that will receive the news.'
+    )
+    args = parser.parse_args()
+
+    if args.sender and args.receiver:
+        extractor = newsExctractor.News_extractor()
+        extractor.extract_news()
+        extractor.send_email(args.sender, args.receiver)
 
 
 if __name__ == "__main__":  # pragma: no cover
